b0a1219771cdfd9897a47b2bf826510ebd8a55f0
evandrvalente <evandr.valente@gmail.com>
2023-06-05 20:54:46 -0300

    feat: functions updated

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const timeH = document.querySelector('h1');
-const timeSecond = 5;
+let timeSecond = 10;
 
-timeH.innerHTML = `00${timeSecond}`;
+displayTime(timeSecond)
 
 const countdown = setInterval(()=>{
   timeSecond--;
+ displayTime(timeSecond)
+  if (timeSecond <= 0 || timeSecond < 1){
+    endTime();
+    clearInterval(countdown);
+  }
 },1000)
+
+function displayTime(second){
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60 );
+  timeH.innerHTML = `${min<10 ? '0' : ''}${min}:${sec<10 ? '0' : ''}${sec}`
+}
+
+function endTime(){
+  timeH.innerHTML = `TIME OUT`
+}
